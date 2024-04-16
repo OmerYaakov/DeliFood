700f8fd444dd9638407020e929afe2f77f7a7052
OmerYaakov <omeryaakov2@gmail.com>
2024-04-16 17:58:44 +0300

    - create the app - creage register page - create login page

---
+++ app/src/main/java/com/example/delifood/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.delifood
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+        val registerButton = findViewById<Button>(R.id.btnRegister)
+        loginButton.setOnClickListener(::onLoginButtonClick)
+        registerButton.setOnClickListener(::onRegisterButtonClick)
+    }
+
+    fun onLoginButtonClick (view: View) {
+        val intent = Intent(this, HomePageActivity::class.java)
+        startActivity(intent)
+    }
+    fun onRegisterButtonClick (view: View) {
+        val intent = Intent(this, RegisterPageActivity::class.java)
+        startActivity(intent)
+    }
+}
+
+
---
+++ app/src/main/java/com/example/delifood/RegisterPageActivity.kt
@@ -0,0 +1,52 @@
+package com.example.delifood
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterPageActivity : AppCompatActivity() {
+    var tvAppName: TextView? = null
+    var etRegisterUsername: EditText? = null
+    var etRegisterEmail: EditText? = null
+    var etRegisterPassword: EditText? = null
+    var imgProfile: ImageView? = null
+    var btnUploadProfile: ImageButton? = null
+    var btnRegister: Button? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+        setupUI()
+        }
+
+
+
+    private fun setupUI(){
+        // Reference to EditText fields
+        etRegisterUsername = findViewById<EditText>(R.id.etRegisterUsername)
+        etRegisterEmail = findViewById<EditText>(R.id.etRegisterEmail)
+        etRegisterPassword = findViewById<EditText>(R.id.etRegisterPassword)
+        imgProfile = findViewById<ImageView>(R.id.imgProfile)
+        btnRegister = findViewById<Button>(R.id.btnRegister)
+        btnUploadProfile = findViewById<ImageButton>(R.id.btnUploadProfile)
+        tvAppName = findViewById<TextView>(R.id.tvAppName)
+
+        //register button
+        btnRegister?.setOnClickListener {
+            val registerUsername = etRegisterUsername?.text.toString()
+            val registerEmail = etRegisterEmail?.text.toString()
+            val registerPassword = etRegisterPassword?.text.toString()
+            val registerImgProfile = imgProfile?.toString()
+
+        }
+    }
+}
